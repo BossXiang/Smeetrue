c2ad59feb3cda221051485fa0ac0118e4d069d4c
Xiang <bosszheng220@gmail.com>
2022-01-20 14:00:54 +0800

    Connect remote database

--- Smeetrue/server/database/mongoose.js
+++ Smeetrue/server/database/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-const dbURL = "mongodb://127.0.0.1:27017/Smeetrue";
+//const dbURL = "mongodb://127.0.0.1:27017/Smeetrue";
+const dbURL =
+  "mongodb+srv://Xiang:abc123456@cluster0.j3hhn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const databaseName = "Smeetrue";
 mongoose
   .connect(dbURL, {
